e90957949c339602c32d8204dbc90d618b1820ab
Hugo Josefson <hugo@josefson.org>
2018-05-10 15:23:02 +0200

    Simplify, clarify.

--- example/apps/ecosystem.config.js
+++ example/apps/ecosystem.config.js
@@ -1,11 +1,13 @@
-const { standardNodeApp } = require('./ecosystem.config.js-helper')
+const { createAppConfig } = require('./ecosystem.config.js-helper')
 
 module.exports = {
     /**
      * Add app configurations here.
+     *
+     * This will not be overwritten by `~/install-nodejs-and-pm2`.
      */
     apps: [
-        standardNodeApp({name: 'example-app', version: '1.0.1', env: {PORT: 3001}}),
-        standardNodeApp({name: 'second-app', version: '1.1.2', env: {PORT: 3002}})
+        createAppConfig({name: 'example-app', version: '1.0.1', env: {PORT: 3001}}),
+        createAppConfig({name: 'second-app', version: '1.1.2', env: {PORT: 3002}})
     ]
 }
